1abb06aee2764c0e987b4fb41158dc2fbee5a61c
z.edd36 <z.edd@36gmail.com>
2022-05-17 18:30:05 -0500

    [Estilo] responsive y retoques

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './estilos/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import apollo from './apollo/index';
